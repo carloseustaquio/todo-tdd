1afff6fddec54d331f9511d85ae5adbf61eac184
Carlos Eustáquio <caeu227@gmail.com>
2020-08-22 16:32:26 -0300

    create route for get todo by id

--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -4,6 +4,7 @@ import * as TodoController from "../controllers/todo.controller"
 const router = Router()
 
 router.post("/", TodoController.createTodo)
-router.get("/", TodoController.getTodo)
+router.get("/", TodoController.getTodos)
+router.get("/:id", TodoController.getTodoById)
 
 export default router 
