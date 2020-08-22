16eed353e79d1d218a3b292c66366001f9903dba
Carlos Eustáquio <caeu227@gmail.com>
2020-08-22 13:47:12 -0300

    create route for get /todos and make integration test for it

--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -4,5 +4,6 @@ import * as TodoController from "../controllers/todo.controller"
 const router = Router()
 
 router.post("/", TodoController.createTodo)
+router.get("/", TodoController.getTodo)
 
 export default router 
