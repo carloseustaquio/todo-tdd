3cec6d9749303ead9c08523955ff023a4b1cd44b
Carlos Eustáquio <caeu227@gmail.com>
2020-08-22 13:38:34 -0300

    create unit tests for find todos and add todo controller method

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,7 +1,20 @@
-import { Request, Response } from "express"
+import { Request, Response, NextFunction } from "express"
 import TodoModel from "../model/todo.model"
 
-export const createTodo = async (req: Request, res: Response) => {
-  const createdModel = await TodoModel.create(req.body)
-  res.status(201).json(createdModel)
+export const createTodo = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const createdModel = await TodoModel.create(req.body)
+    res.status(201).json(createdModel)
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const getTodo = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const allTodos = await TodoModel.find({})
+    res.status(200).json(allTodos)
+  } catch (err) {
+    next(err)
+  }
 }
--- src/test/unit/todo.controller.test.ts
+++ src/test/unit/todo.controller.test.ts
@@ -3,14 +3,51 @@ import TodoModel from "../../model/todo.model"
 import httpMocks, { MockRequest, MockResponse } from "node-mocks-http"
 // mock data
 import newTodo from "../mock-data/new-todo.json"
+import allTodos from "../mock-data/all-todos.json"
 
-TodoModel.create = jest.fn() // Mock/Spy Function
+TodoModel.create = jest.fn() as jest.Mock // Mock/Spy Function
+TodoModel.find = jest.fn() as jest.Mock
 
-let req: MockRequest<any>, res: MockResponse<any>
+let req: MockRequest<any>, res: MockResponse<any>, next: jest.Mock
 
 beforeEach(() => {
   req = httpMocks.createRequest()
   res = httpMocks.createResponse()
+  next = jest.fn()
+})
+
+describe("TodoController.getTodos", () => {
+
+  it("should hava a getTodo function", () => {
+    // Test done just to follow the course, 
+    // TypeScript automatically test this for me!
+    expect(typeof TodoController.getTodo).toBe("function")
+  })
+
+  it("should call TodoModel.find({})", async () => {
+    await TodoController.getTodo(req, res, next)
+    expect(TodoModel.find).toBeCalled()
+  })
+
+  it("should return response with status 200", async () => {
+    await TodoController.getTodo(req, res, next)
+    expect(res.statusCode).toBe(200)
+    expect(res._isEndCalled()).toBeTruthy()
+  })
+
+  it("should return response with all todos", async () => {
+    (TodoModel.find as jest.Mock).mockReturnValue(allTodos)
+    await TodoController.getTodo(req, res, next)
+    expect(res._getJSONData()).toStrictEqual(allTodos)
+  })
+
+  it("should handle errors in getTodos", async () => {
+    const errorMessage = { message: "Error finding" }
+    const rejectedPromise = Promise.reject(errorMessage);
+    (TodoModel.find as jest.Mock).mockReturnValue(rejectedPromise);
+    await TodoController.getTodo(req, res, next)
+    expect(next).toHaveBeenCalledWith(errorMessage)
+  })
 })
 
 describe("TodoController.createTodo", () => {
@@ -24,19 +61,27 @@ describe("TodoController.createTodo", () => {
   })
 
   it("should call TodoModel.create", () => {
-    TodoController.createTodo(req, res)
+    TodoController.createTodo(req, res, next)
     expect(TodoModel.create).toBeCalledWith(newTodo)
   })
 
   it("should return 201 response code", async () => {
-    await TodoController.createTodo(req, res)
+    await TodoController.createTodo(req, res, next)
     expect(res.statusCode).toBe(201)
     expect(res._isEndCalled()).toBeTruthy()
   })
 
   it("should return json body in response", async () => {
     (TodoModel.create as jest.Mock).mockReturnValue(newTodo)
-    await TodoController.createTodo(req, res)
+    await TodoController.createTodo(req, res, next)
     expect(res._getJSONData()).toStrictEqual(newTodo)
+  })
+
+  it("should handle errors", async () => {
+    const errorMessage = { message: "Done property missing" };
+    const rejectedPromise = Promise.reject(errorMessage);
+    (TodoModel.create as jest.Mock).mockReturnValue(rejectedPromise)
+    await TodoController.createTodo(res, res, next)
+    expect(next).toBeCalledWith(errorMessage)
   })
 })
